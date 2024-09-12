eeac147b6e81dbddb2b1e1551ddb9bb0ca335cea
Mohammed  Hamdy <45946756+MohamedHamdy98@users.noreply.github.com>
2024-09-12 18:02:13 +0300

    User_inputs_port_1

--- UserInputs/user_inputs.py
+++ UserInputs/user_inputs.py
@@ -124,4 +124,4 @@ def get_uploaded_data():
     return jsonify(metadata), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=1)
