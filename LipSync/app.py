ddc7625bdd4a6bf93ded1b226c9500c374d8dae1
Mohammed  Hamdy <45946756+MohamedHamdy98@users.noreply.github.com>
2024-09-12 17:59:13 +0300

    LipSync V2

--- LipSync/app.py
+++ LipSync/app.py
@@ -44,5 +44,6 @@ def upload_file():
 
     return jsonify({'error': 'Invalid file type'}), 400
 
+# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Run the Flask app
+    app.run(debug=True, host='0.0.0.0', port=5000)  
